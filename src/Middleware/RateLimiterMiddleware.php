f6be0c162bb7a1c4b9ceba665a75123e1f340f71
Avinash Rathod <28943172+Avnsh1111@users.noreply.github.com>
2023-04-24 15:02:44 +0530

    Create RateLimiterMiddleware.php

---
+++ src/Middleware/RateLimiterMiddleware.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Avnsh1111\LaravelApiRateLimiter\Middleware;
+
+use Closure;
+use Avnsh1111\LaravelApiRateLimiter\RateLimiter;
+use Illuminate\Http\Response;
+
+class RateLimiterMiddleware
+{
+    protected $rateLimiter;
+
+
+    public function __construct(RateLimiter $rateLimiter)
+    {
+        $this->rateLimiter = $rateLimiter;
+    }
+
+    public function handle($request, Closure $next)
+    {
+        $maxAttempts = config('api_rate_limiter.default_limit', 60);
+        $decayMinutes = 1;
+
+        $ip = $request->ip();
+        $userId = $request->user() ? $request->user()->id : null;
+        $route = $request->route() ? $request->route()->getName() : null;
+
+        if ($this->rateLimiter->isWhitelisted($ip, $userId, $route)) {
+            return $next($request);
+        }
+
+        if ($this->rateLimiter->isBlacklisted($ip, $userId, $route)) {
+            return response('Too many requests.', 429);
+        }
+
+        if ($this->rateLimiter->tooManyAttempts($request, $maxAttempts, $decayMinutes)) {
+            $response = new Response('Too Many Attempts', 429);
+
+            if (config('api_rate_limiter.response_headers', true)) {
+                $headers = [
+                    'X-RateLimit-Limit' => $maxAttempts,
+                    'X-RateLimit-Remaining' => $this->rateLimiter->remainingAttempts($request, $maxAttempts),
+                    'X-RateLimit-Reset' => $this->rateLimiter->resetTime($request),
+                ];
+
+                $response->headers->add($headers);
+            }
+
+            return $response;
+        }
+
+        $response = $next($request);
+
+        if (config('api_rate_limiter.response_headers', true)) {
+            $headers = [
+                'X-RateLimit-Limit' => $maxAttempts,
+                'X-RateLimit-Remaining' => $this->rateLimiter->remainingAttempts($request, $maxAttempts) - 1,
+                'X-RateLimit-Reset' => $this->rateLimiter->resetTime($request),
+            ];
+
+            $response->headers->add($headers);
+        }
+
+        $this->rateLimiter->hit($request, $decayMinutes);
+
+        return $response;
+    }
+
+
+}
