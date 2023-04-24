618159891ae4ec257570e1546f2151606f469390
Avinash Rathod <28943172+Avnsh1111@users.noreply.github.com>
2023-04-24 15:03:28 +0530

    Create RateLimiterServiceProvider.php

---
+++ src/RateLimiterServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Avnsh1111\LaravelApiRateLimiter;
+
+use Illuminate\Support\ServiceProvider;
+
+class RateLimiterServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/api_rate_limiter.php' => config_path('api_rate_limiter.php'),
+        ], 'config');
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/api_rate_limiter.php', 'api_rate_limiter'
+        );
+    }
+}
